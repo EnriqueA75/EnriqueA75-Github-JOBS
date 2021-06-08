13a539c26646a02700ae581ecfbfd9665777bba3
EnriqueA75 <48929949+EnriqueA75@users.noreply.github.com>
2021-06-07 18:33:18 -0600

    fake/login disable

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -19,7 +19,7 @@ const client_secret = 'ac701872b2d625af079bbcb636c2f2e9fbd353c9';
 
 export const useLogin = () => {
   const [isUnmount, setIsUnmount] = useState(false);
-  const [isAuthenticated, setIsAuthenticated] = useState(true);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [isRequesting, setIsRequesting] = useState(false);
 
   const [, response, promptAsync] = useAuthRequest(
